fb9e017146a0168cef4835633a6a8fbdfc197903
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-04-04 10:18:38 +0300

    Add project files.

---
+++ Oop101/Kisi.cs
@@ -0,0 +1,42 @@
+﻿namespace Oop101
+{
+    public class Kisi 
+    {
+        //Access Modifiers
+        /*
+         * private; sadece tanımlandığı scope içinden erişilebilir
+         * internal; sadece aynı namespace(proje) içerisinden erişilebilir
+         * public; referans verilen tüm projelerden erişilebilir
+         * protected; sadece kalıtım içerisinden erişilebilir
+         * protected internal; sadece kalıtımdan ve aynı namespace(proje) den erişilebilir
+         */
+        
+
+        private string _ad; //field
+        private string _soyad;
+        private DateTime _dogumTarihi;
+
+        public string Ad //full-property
+        {
+            get => _ad.ToUpper();
+            set
+            {
+                foreach (char harf in value)
+                {
+                    if (char.IsDigit(harf))
+                        throw new Exception("Adınızda rakam bulunamaz");
+                }
+                _ad = value;
+            }
+        }
+        public string Soyad //full-property
+        {
+            get => _soyad;
+            set => _soyad = value;
+        }
+
+        public DateTime DogumTarihi { get; set; } //auto-property
+
+        public int Yas => DateTime.Now.Year - _dogumTarihi.Year; // read-only property
+    }
+}
---
+++ Oop101/Program.cs
@@ -0,0 +1,18 @@
+﻿using Oop101;
+
+var tarih = DateTime.Now;
+var rastgeleSayi = new Random().Next(1, 100);
+
+try
+{
+    var kisi1 = new Kisi
+    {
+        Ad = "123132132",
+        Soyad = "Fıdıl"
+    };
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    
+}
