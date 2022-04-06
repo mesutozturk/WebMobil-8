450a5a43d89671721a805329c2e06c189ad10b4b
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-04-06 11:12:07 +0300

    .

--- Oop101.Polymorphism/Program.cs
+++ Oop101.Polymorphism/Program.cs
@@ -53,7 +53,7 @@ foreach (Sekil item in sekiller)
     if (item is Kare) Console.WriteLine("Kare");
     else if (item is Ucgen) Console.WriteLine("Üçgen");
     else if (item is DikDortgen) Console.WriteLine("Dikdörtgen");
-    
+
     Console.WriteLine($"Alan: {item.AlanHesapla()}m²");
     Console.WriteLine($"Çevresi: {item.CevreHesapla()}m");
 }
@@ -79,3 +79,32 @@ foreach (Sekil item in sekiller)
  * müşterilerin bakiyeleri hesaplanabilecek
  * 
  */
+
+Sekiller seciliSekil = Sekiller.Ucgen;
+
+var uretilenSekil = SekilFactory(seciliSekil);
+
+Sekil SekilFactory(Sekiller sekil)
+{
+    Sekil model;
+    switch (sekil)
+    {
+        case Sekiller.Kare:
+            model = new Kare();
+            break;
+        case Sekiller.DikDortgen:
+            model = new DikDortgen();
+            break;
+        case Sekiller.Ucgen:
+            model = new Ucgen();
+            break;
+        default:
+            throw new ArgumentOutOfRangeException(nameof(sekil), sekil, null);
+    }
+
+    return model;
+}
+
+Stack<Sekil> sekilStack = new(); //lifo
+Queue<Sekil> sekilQueue = new(); //fifo
+
