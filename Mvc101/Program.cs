3ec64e9d044f8f9810e9ab310403562239b92452
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-04-07 12:12:14 +0300

    DI and Loose Coupling

--- Mvc101/Program.cs
+++ Mvc101/Program.cs
@@ -1,6 +1,10 @@
+using Mvc101.Services.SmsService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ISmsService, WissenSmsService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
---
+++ Mvc101/Services/SmsService/WissenSmsService.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Mvc101.Models;
+
+namespace Mvc101.Services.SmsService
+{
+    public class WissenSmsService : ISmsService
+    {
+        public SmsStates Send(SmsModel model)
+        {
+            Debug.Write($"Wissen: {model.TelefonNo} - {model.Mesaj}");
+            return SmsStates.Sent;
+        }
+    }
+}
