6200fba76cb495b26fec978d8293b9d621db7640
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-04-07 14:32:31 +0300

    = new();

--- Mvc101/Models/MailModel.cs
+++ Mvc101/Models/MailModel.cs
@@ -2,10 +2,10 @@
 
 public class MailModel
 {
-    public List<EmailModel> To { get; set; }
-    public List<EmailModel> Cc { get; set; }
-    public List<EmailModel> Bcc { get; set; }
+    public List<EmailModel> To { get; set; } = new();
+    public List<EmailModel> Cc { get; set; } = new();
+    public List<EmailModel> Bcc { get; set; } = new();
     public string Subject { get; set; }
     public string Body { get; set; }
-    public List<Stream> Attachs { get; set; }
+    public List<Stream> Attachs { get; set; } 
 }
