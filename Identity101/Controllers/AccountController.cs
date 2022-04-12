8c2924b4c64caac27a4b90b9ec8428be1bb7b836
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-12 11:55:44 +0300

    confirm email

--- Identity101/Controllers/AccountController.cs
+++ Identity101/Controllers/AccountController.cs
@@ -93,9 +93,36 @@ public class AccountController : Controller
             return RedirectToAction("Login");
         }
 
-        var messages = string.Join("\n", result.Errors.Select(x => x.Description));
+        var messages = string.Join("<br>", result.Errors.Select(x => x.Description));
         ModelState.AddModelError(string.Empty, messages);
         return View(model);
+    }
+
+    public async Task<IActionResult> ConfirmEmail(string userId, string code)
+    {
+        if (userId == null || code == null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with ID '{userId}'.");
+        }
+
+        code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+        var result = await _userManager.ConfirmEmailAsync(user, code);
+        ViewBag.StatusMessage =
+            result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+
+        if (result.Succeeded && _userManager.IsInRoleAsync(user, Roles.Passive).Result)
+        {
+            await _userManager.RemoveFromRoleAsync(user, Roles.Passive);
+            await _userManager.AddToRoleAsync(user, Roles.User);
+        }
+
+        return View();
     }
 
     public IActionResult Login()
