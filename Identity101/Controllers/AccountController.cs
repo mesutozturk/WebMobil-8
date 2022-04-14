46eee357b32818dcb90d39e83176d3ff295b2c88
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-14 10:14:35 +0300

    change email

--- Identity101/Controllers/AccountController.cs
+++ Identity101/Controllers/AccountController.cs
@@ -278,16 +278,41 @@ public class AccountController : Controller
         return View(model);
     }
 
-    [Authorize]
-    [HttpPost]
+    [Authorize, HttpPost]
     public async Task<IActionResult> Profile(UserProfileViewModel model)
     {
+        if (!ModelState.IsValid)
+            return View(model);
         var user = await _userManager.FindByNameAsync(HttpContext.User.Identity!.Name);
-
         user.Name = model.Name;
         user.Surname = model.Surname;
-        user.Email = model.Email;
 
+        bool isAdmin = await _userManager.IsInRoleAsync(user, Roles.Admin);
+        if (!isAdmin && user.Email != model.Email)
+        {
+            await _userManager.RemoveFromRoleAsync(user, Roles.User);
+            await _userManager.AddToRoleAsync(user, Roles.Passive);
+            user.EmailConfirmed = false;
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            var emailMessage = new MailModel()
+            {
+                To = new List<EmailModel>
+                {
+                    new() { Adress = model.Email, Name = user.Name }
+                },
+                Body =
+                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here </a>.",
+                Subject = "Confirm your email"
+            };
+            await _emailService.SendMailAsync(emailMessage);
+        }
+
+        user.Email = model.Email;
         var result = await _userManager.UpdateAsync(user);
         if (result.Succeeded)
         {
