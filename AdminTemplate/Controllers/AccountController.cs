0b15c8812f54af22f2e425290b136440f8a16e2d
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-20 10:14:02 +0300

    register post

--- AdminTemplate/Controllers/AccountController.cs
+++ AdminTemplate/Controllers/AccountController.cs
@@ -1,9 +1,13 @@
+using System.Text;
+using System.Text.Encodings.Web;
+using AdminTemplate.Models.Email;
 using AdminTemplate.Models.Identity;
 using AdminTemplate.Models.Role;
 using AdminTemplate.Services.Email;
 using AdminTemplate.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace AdminTemplate.Controllers;
 
@@ -38,9 +42,54 @@ public class AccountController : Controller
     }
 
     [HttpPost]
-    public IActionResult Register(RegisterViewModel model)
+    public async Task<IActionResult> Register(RegisterViewModel model)
     {
-        
-        return View();
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty, "Bir hata oluştu");
+            return View(model);
+        }
+
+        var user = new ApplicationUser
+        {
+            UserName = model.UserName,
+            Email = model.Email,
+            Name = model.Name,
+            Surname = model.Surname
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            //Rol Atama
+            var count = _userManager.Users.Count();
+            result = await _userManager.AddToRoleAsync(user, count == 1 ? Roles.Admin : Roles.Passive);
+
+            //Email gönderme - Aktivasyon
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            var email = new MailModel()
+            {
+                To = new List<EmailModel>
+                {
+                    new EmailModel()
+                        { Adress = user.Email, Name = user.UserName }
+                },
+                Body =
+                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.",
+                Subject = "Confirm your email"
+            };
+
+            await _emailService.SendMailAsync(email);
+            //TODO: Login olma
+            return RedirectToAction("Login");
+        }
+
+        var messages = string.Join("<br>", result.Errors.Select(x => x.Description));
+        ModelState.AddModelError(string.Empty, messages);
+        return View(model);
     }
 }
