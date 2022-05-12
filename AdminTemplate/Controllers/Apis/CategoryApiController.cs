7ca69a15dc7dcc8260c517c76ec41cbd95851537
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-05-12 11:50:43 +0300

    .

--- AdminTemplate/Controllers/Apis/CategoryApiController.cs
+++ AdminTemplate/Controllers/Apis/CategoryApiController.cs
@@ -3,7 +3,6 @@ using AdminTemplate.Dtos;
 using AdminTemplate.Models.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace AdminTemplate.Controllers.Apis
 {
